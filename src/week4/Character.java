bf6bafee9314f2efe1dcbde7735140850119a335
pauled <drpauled@gmail.com>
2024-02-16 12:51:04 -0500

    week4-3

--- src/week4/Character.java
+++ src/week4/Character.java
@@ -12,4 +12,9 @@ public class Character {
         this.battlesWon++;
         this.expPts+=xp;
     }
+    public String toString(){
+        String out="battlesWon: "+this.battlesWon;
+        out+=" xp: "+this.expPts;
+        return out;
+    }
 }
--- src/week4/Party.java
+++ src/week4/Party.java
@@ -19,4 +19,22 @@ public class Party {
             this.members.get(x).winBattles(xp);
         }
     }
+    public String toString(){
+        String out="battles won: "+this.battlesWon;
+        out+="\nmembers: \n";
+        for (int x=0;x<this.members.size(); x++){
+            out+=" "+this.members.get(x)+"\n";
+        }
+        return out;
+    }
+    public static void main(String[] args) {
+        Character hero=new Character();
+        Character hero2=new Character();
+        hero.winBattles(10);
+        Party party=new Party();
+        party.addMembers(hero);
+        party.addMembers(hero2);
+        party.winBattle(20);
+        System.out.println(party);
+    }
 }
