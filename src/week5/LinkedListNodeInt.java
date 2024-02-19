a873298332fe5bdf0f8dd6d1b159fc1922fd347e
pauled <drpauled@gmail.com>
2024-02-19 12:51:22 -0500

    week5-1

---
+++ src/week5/LinkedListNodeInt.java
@@ -0,0 +1,36 @@
+package week5;
+
+public class LinkedListNodeInt {
+    private int value;
+    private LinkedListNodeInt next;
+
+    public LinkedListNodeInt(int value,LinkedListNodeInt next){
+        this.value=value;
+        this.next=next;
+    }
+
+    @Override
+    public String toString() {
+        String out="";
+        if (this.next!=null){
+            out+=this.next.toString()+" ";
+        }
+        out+=this.value;
+        return out;
+    }
+    public String toString2() {
+        if (this.next==null){
+            return ""+this.value;
+        } else {
+            return this.next.toString2()+" "+this.value;
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedListNodeInt first=new LinkedListNodeInt(1,null);
+        first=new LinkedListNodeInt(2,first);
+        first=new LinkedListNodeInt(3,first);
+        String temp=first.toString();
+        System.out.println(temp);
+    }
+}
